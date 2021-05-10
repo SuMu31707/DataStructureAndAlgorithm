d8ab225a96798bf50b3a037d6f3c49b112aeb133
SuMu <SuMu31707@163.com>
2021-05-10 17:13:31 +0800

    两数之和（升序数组）-> 双指针算法

--- Practice/src/com/sumu/TwoSumByOrder.java
+++ Practice/src/com/sumu/TwoSumByOrder.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 public class TwoSumByOrder {
     public static void main(String[] args) {
         System.out.println("二分查找算法：" + Arrays.toString(binarySearch(new int[]{1, 2, 3, 4, 5, 6}, 10)));
+        System.out.println("双指针算法：" + Arrays.toString(twoPoint(new int[]{1, 2, 3, 4, 5, 6}, 10)));
     }
 
     // 二分法
@@ -29,6 +30,25 @@ public class TwoSumByOrder {
                 } else {
                     low = mid + 1;
                 }
+            }
+        }
+        return res;
+    }
+
+    // 双指针算法
+    public static int[] twoPoint(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[low] + nums[high];
+            if (sum == target) {
+                res[0] = low;
+                res[1] = high;
+                return res;
+            } else if (sum > target) {
+                high--;
+            } else {
+                low++;
             }
         }
         return res;
