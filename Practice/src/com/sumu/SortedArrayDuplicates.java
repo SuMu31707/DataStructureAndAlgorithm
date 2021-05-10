759da4788f5ca25c3878d4fdcd257f3a96635e25
SuMu <SuMu31707@163.com>
2021-05-10 10:41:24 +0800

    删除排序数组中的重复项

--- Practice/src/com/sumu/SortedArrayDuplicates.java
+++ Practice/src/com/sumu/SortedArrayDuplicates.java
@@ -9,7 +9,21 @@ package com.sumu;
  */
 public class SortedArrayDuplicates {
     public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{0, 1, 2, 2, 3, 3, 4}));
+    }
 
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 1 || nums == null) {
+            return nums.length;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
     }
 
 
