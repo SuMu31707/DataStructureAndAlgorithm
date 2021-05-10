aeb5a61f35756c2e300d02d6dab6e76078ad3356
SuMu <SuMu31707@163.com>
2021-05-10 09:30:14 +0800

    埃塞法->统计素数个数

--- Practice/src/com/sumu/SuShu.java
+++ Practice/src/com/sumu/SuShu.java
@@ -1,8 +1,15 @@
 package com.sumu;
 
+/**
+ * 题目：统计n以内的素数个数
+ * 素数：只能被1和自身整除的自然数，0和1除外
+ * 例如：输入100，输出25
+ * 考察重点：埃塞法
+ */
 public class SuShu {
     public static void main(String[] args) {
         System.out.println("暴力算法统计结果：" + bf(100));
+        System.out.println("暴力算法统计结果：" + eratosthenes(100));
     }
 
     // 暴力算法统计素数个数
@@ -14,7 +21,6 @@ public class SuShu {
         return count;
     }
 
-
     private static boolean isPrime(int x) {
         // 将i<n优化成i*i<n，只判断到根号x
         for (int i = 2; i * i <= x; i++) {
@@ -24,4 +30,24 @@ public class SuShu {
         }
         return true;
     }
+
+    /**
+     * 埃塞法统计素数个数
+     * 素数  非素数（合数） 12 = 2 * 6
+     */
+    public static int eratosthenes(int n) {
+        // 建立标记位数组，数组中每个元素默认为false，此处false代表素数
+        boolean[] isPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!isPrime[i]) {
+                count++;
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = true;
+                }
+            }
+        }
+        return count;
+    }
+
 }
